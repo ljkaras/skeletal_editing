7fcb025d498e17c80c94d8c164d21c12c2d6ce2d
lucaskaras@gmail.com <lucaskaras@gmail.com>
2024-03-15 21:57:33 -0600

    updates

--- parallel_batch.py
+++ parallel_batch.py
@@ -19,8 +19,7 @@ def process_batch(batch, substructure):
     with multiprocessing.Pool() as p:
         return p.starmap(process_line, [(line, substructure) for line in batch])
 
-def main(lines, substructure, failed_csv, aromatic_csv, nonaromatic_csv, batch_size=1000000):
-    time_start = time.time()
+def main(lines, substructure, failed_csv, aromatic_csv, nonaromatic_csv, batch_size=100000):
 
     # Process results
     with open(failed_csv, 'w', newline='') as failed_file, \
@@ -36,7 +35,11 @@ def main(lines, substructure, failed_csv, aromatic_csv, nonaromatic_csv, batch_s
         nonaromatic_writer.writerow(['SMILES', 'ID'])
 
         # Process tasks in batches
-        for i in range(1, len(lines), batch_size):  # Skip the header row in the first batch
+        total_batches = (len(lines) - 1) // batch_size + 1  # Calculate the total number of batches
+
+        for batch_num, i in enumerate(range(1, len(lines), batch_size), start=1):  # Skip the header row in the first batch
+            time_start = time.time()
+            print(f"Processing batch {batch_num} of {total_batches}.")
             batch = lines[i:i + batch_size]
             results = process_batch(batch, substructure)
 
@@ -47,21 +50,41 @@ def main(lines, substructure, failed_csv, aromatic_csv, nonaromatic_csv, batch_s
                     aromatic_writer.writerow([smiles, mol_id])
                 else:
                     nonaromatic_writer.writerow([smiles, mol_id])
-
-    time_end = time.time()
-    print(f"Processing time: {time_end - time_start:.2f} seconds")
+            time_end = time.time()
+            print(f"Processing time: {time_end - time_start:.2f} s for {batch_num} of {total_batches}")
     
+import sys
+
 if __name__ == '__main__':
-    filename = "../test.cxsmiles"
+    if len(sys.argv) != 2:
+        print("Usage: python parallel_batch.py <filename>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
     print(f"Reading {filename}")
     with open(filename, 'r') as infile:
         lines = infile.readlines()
     print(f'Found {len(lines)} SMILES strings')
+    base_filename = filename.rsplit('.', 1)[0]
+    print(base_filename)
     substructure = Chem.MolFromSmarts('[a]')
-    failed_csv = "../failed.csv"
-    aromatic_csv = "../aromatic.csv"
-    nonaromatic_csv = "../nonaromatic.csv"
+    failed_csv = base_filename + "_failed.csv"
+    aromatic_csv = base_filename + "_aromatic.csv"
+    nonaromatic_csv = base_filename + "_nonaromatic.csv"
     
     main(lines, substructure, failed_csv, aromatic_csv, nonaromatic_csv)
+
+# if __name__ == '__main__':
+#     filename = "../test.cxsmiles"
+#     print(f"Reading {filename}")
+#     with open(filename, 'r') as infile:
+#         lines = infile.readlines()
+#     print(f'Found {len(lines)} SMILES strings')
+#     substructure = Chem.MolFromSmarts('[a]')
+#     failed_csv = "../failed.csv"
+#     aromatic_csv = "../aromatic.csv"
+#     nonaromatic_csv = "../nonaromatic.csv"
+    
+#     main(lines, substructure, failed_csv, aromatic_csv, nonaromatic_csv)
 
 #81.54567885398865 seconds
