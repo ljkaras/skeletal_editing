812241d79eb053bd4a9995d3c7a6190627a65915
lucaskaras@gmail.com <lucaskaras@gmail.com>
2024-03-17 20:08:14 -0600

    mar1724

--- frameworks/filter_2.py
+++ frameworks/filter_2.py
@@ -3,11 +3,12 @@ import multiprocessing
 import csv
 import time
 
-def process_line(line, sub_pyridine, sub_pyridine14H, sub_pyrimidine14):
+def process_line(line, sub_pyridine, sub_pyridine14H, sub_pyrimidine14, sub_test):
     line = line.strip().split(',')
     # print(f"line: {line}")
     smiles, mol_id = line[0], line[1]
     mol = Chem.MolFromSmiles(smiles)
+    mol = Chem.AddHs(mol)
     if mol.HasSubstructMatch(sub_pyridine):
         if mol.HasSubstructMatch(sub_pyridine14H):
             return smiles, mol_id, True, 2
@@ -15,12 +16,14 @@ def process_line(line, sub_pyridine, sub_pyridine14H, sub_pyrimidine14):
             return smiles, mol_id, True, 1
     elif mol.HasSubstructMatch(sub_pyrimidine14):
         return smiles, mol_id, False, 3
+    elif mol.HasSubstructMatch(sub_test):
+        return smiles, mol_id, False, 4
     else:
         return smiles, mol_id, False, 0
  
-def process_batch(batch, sub_pyridine, sub_pyridine14H, sub_pyrimidine14):
+def process_batch(batch, sub_pyridine, sub_pyridine14H, sub_pyrimidine14, sub_test,):
     with multiprocessing.Pool() as p:
-        return p.starmap(process_line, [(line, sub_pyridine, sub_pyridine14H, sub_pyrimidine14) for line in batch])
+        return p.starmap(process_line, [(line, sub_pyridine, sub_pyridine14H, sub_pyrimidine14, sub_test) for line in batch])
 
 def main(lines, pyridine_csv, pyridine14H_csv, pyrimidine14_csv, batch_size=100000):
     # Process results
@@ -44,7 +47,7 @@ def main(lines, pyridine_csv, pyridine14H_csv, pyrimidine14_csv, batch_size=1000
             time_start = time.time()
             print(f"Processing batch {batch_num} of {total_batches}.")
             batch = lines[i:i + batch_size]
-            results = process_batch(batch, sub_pyridine, sub_pyridine14H, sub_pyrimidine14)
+            results = process_batch(batch, sub_pyridine, sub_pyridine14H, sub_pyrimidine14, sub_test)
             # print(results)
             for smiles, mol_id, pyridine, state in results:
                 if pyridine:
@@ -53,6 +56,8 @@ def main(lines, pyridine_csv, pyridine14H_csv, pyrimidine14_csv, batch_size=1000
                         pyridine14H_writer.writerow([smiles, mol_id])
                 elif state == 3:
                     pyrimidine14_writer.writerow([smiles, mol_id])
+                elif state == 4:
+                    print([smiles, mol_id])
 
             time_end = time.time()
             print(f"Processing time: {time_end - time_start:.2f} s for {batch_num} of {total_batches}")
@@ -76,6 +81,7 @@ if __name__ == '__main__':
     # sub_pyridine13H = Chem.MolFromSmarts("c1c[cH]c[n;D2]c1")
     # sub_pyrimidine13 = Chem.MolFromSmarts("c1[n;D2]c[n;D2]cc1")
     sub_pyrimidine14 = Chem.MolFromSmarts("c1[n;D2]cc[n;D2]c1")
+    sub_test = Chem.MolFromSmarts("C1=CC=CNC=C1")     
     pyridine_csv = base_filename + "_pyridine.csv"
     pyridine14H_csv = base_filename + "_pyridine14H.csv"
     pyrimidine14_csv = base_filename + "_pyrimidine14.csv"
